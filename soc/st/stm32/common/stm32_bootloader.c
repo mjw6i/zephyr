453fec1e96f5da6a2c787a22f365d96c9796f3c8
mjw6i <91162351+mjw6i@users.noreply.github.com>
2026-09-15 20:10:23 +0200

    try without __

--- soc/st/stm32/common/stm32_bootloader.c
+++ soc/st/stm32/common/stm32_bootloader.c
@@ -41,7 +41,8 @@ static FUNC_NORETURN void jump_to_bootloader(void)
 	SysTick->CTRL = 0;
 
 	// LL_SYSCFG_SetRemapMemory(LL_SYSCFG_REMAP_SYSTEMFLASH);
-	__HAL_SYSCFG_REMAPMEMORY_SYSTEMFLASH();
+	// __HAL_SYSCFG_REMAPMEMORY_SYSTEMFLASH();
+	HAL_SYSCFG_REMAPMEMORY_SYSTEMFLASH();
 
 	jmp = (void (*)(void))(void (*)(void))(*((uint32_t *)((bootloader + 4))));
 
